7089e1dd37f9fbc0ccc61b43c63bd0e834548a9c
edilson <eddylson.londonio@gmail.com>
2020-12-02 21:44:53 -0500

    crud seller

--- coffee-api/routes/coffee-seller.js
+++ coffee-api/routes/coffee-seller.js
@@ -1,11 +1,68 @@
 const router = require('express').Router()
+const config = require('../config/config')['dev']
 const coffeeSellerController = require('../controller/coffee-seller')
+const db = require('coffee-db')
+let service, Seller
+
+router.use('*', async (req,res,next)=>{
+	if (!service){
+		try{
+			service = await db(config)
+			console.log(service)
+		}catch(e){
+			console.log('error', e)
+			return e
+		}
+		Seller = service.ModelSeller
+	}
+	next()
+})
+
+router.post('/coffee-seller', async(req,res)=>{
+	//console.log('seller here', Seller)
+	let { name , password , email } = req.body
+	if (!name || !password || !email) return res.status(404).send({
+		message : 'Faltan argumentos',
+		ok: false
+	})
+	const seller = await Seller.create({
+		name,
+		password,
+		email
+	})
+	
+	return res.status(200).send({
+		mensaje: 'success',
+		ok:true,
+		vendedor : seller.toJSON()
+	})
+})
+
+router.get('/coffee-seller', async(req,res) => {
+	let sellers = await Seller.findAll()
+	return res.status(200).send({
+		ok: true,
+		vendedores : sellers
+	})
+})
+
+router.get('/coffee-seller/:id', async(req,res)=>{
+	const {id} = req.params
+	const seller = await Seller.findOne({
+		where : {
+			id: id
+		}
+	})
+	return res.status(200).send({
+
+		ok: true,
+		vendedor: seller
+
+	})
+})
 
 
-router.get('/coffee-seller', coffeeSellerController.createSellerCoffee)
-
-
-
+// TODO : routes for delete and update
 module.exports = {
 	router
 }
